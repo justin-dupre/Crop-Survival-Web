9cf23cd88bdb0c575e460b7191bf76c8b30be1e5
justin-dupre <42753529+justin-dupre@users.noreply.github.com>
2023-02-01 20:35:30 -0600

    init

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,11 @@
-import '@/styles/globals.css'
+import '@/styles/globals.css';
+import { VT323 } from '@next/font/google';
+const vt323 = VT323({ subsets: ['latin'], weight: '400' });
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <main className={vt323.className}>
+      <Component {...pageProps} />
+    </main>
+  );
 }
